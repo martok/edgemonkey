b17c7131f84e0b8acddb3df5f6fea31ad97d4039
Martok <martok@martoks-place.de>
2010-06-25 23:10:28 +0200

    add: Date formatting
    Signed-off-by: Martok <martok@martoks-place.de>

--- edgemonkey.user.js
+++ edgemonkey.user.js
@@ -436,6 +436,71 @@ RegExp.prototype.execAll=function(data) {
     return null;
 }
 
+//http://jacwright.com/projects/javascript/date_format
+// Simulates PHP's date function
+Date.prototype.format = function(format) {
+  var returnStr = '';
+  var replace = {
+    shortMonths: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    longMonths: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    shortDays: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    longDays: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+
+    // Day
+    d: function() { return (this.getDate() < 10 ? '0' : '') + this.getDate(); },
+    D: function() { return replace.shortDays[this.getDay()]; },
+    j: function() { return this.getDate(); },
+    l: function() { return replace.longDays[this.getDay()]; },
+    N: function() { return this.getDay() + 1; },
+    S: function() { return (this.getDate() % 10 == 1 && this.getDate() != 11 ? 'st' : (this.getDate() % 10 == 2 && this.getDate() != 12 ? 'nd' : (this.getDate() % 10 == 3 && this.getDate() != 13 ? 'rd' : 'th'))); },
+    w: function() { return this.getDay(); },
+    z: function() { return "Not Yet Supported"; },
+    // Week
+    W: function() { return "Not Yet Supported"; },
+    // Month
+    F: function() { return replace.longMonths[this.getMonth()]; },
+    m: function() { return (this.getMonth() < 9 ? '0' : '') + (this.getMonth() + 1); },
+    M: function() { return replace.shortMonths[this.getMonth()]; },
+    n: function() { return this.getMonth() + 1; },
+    t: function() { return "Not Yet Supported"; },
+    // Year
+    L: function() { return (((this.getFullYear()%4==0)&&(this.getFullYear()%100 != 0)) || (this.getFullYear()%400==0)) ? '1' : '0'; },
+    o: function() { return "Not Supported"; },
+    Y: function() { return this.getFullYear(); },
+    y: function() { return ('' + this.getFullYear()).substr(2); },
+    // Time
+    a: function() { return this.getHours() < 12 ? 'am' : 'pm'; },
+    A: function() { return this.getHours() < 12 ? 'AM' : 'PM'; },
+    B: function() { return "Not Yet Supported"; },
+    g: function() { return this.getHours() % 12 || 12; },
+    G: function() { return this.getHours(); },
+    h: function() { return ((this.getHours() % 12 || 12) < 10 ? '0' : '') + (this.getHours() % 12 || 12); },
+    H: function() { return (this.getHours() < 10 ? '0' : '') + this.getHours(); },
+    i: function() { return (this.getMinutes() < 10 ? '0' : '') + this.getMinutes(); },
+    s: function() { return (this.getSeconds() < 10 ? '0' : '') + this.getSeconds(); },
+    // Timezone
+    e: function() { return "Not Yet Supported"; },
+    I: function() { return "Not Supported"; },
+    O: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + '00'; },
+    P: function() { return (-this.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(this.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() / 60)) + ':' + (Math.abs(this.getTimezoneOffset() % 60) < 10 ? '0' : '') + (Math.abs(this.getTimezoneOffset() % 60)); },
+    T: function() { var m = this.getMonth(); this.setMonth(0); var result = this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/, '$1'); this.setMonth(m); return result;},
+    Z: function() { return -this.getTimezoneOffset() * 60; },
+    // Full Date/Time
+    c: function() { return this.format("Y-m-d") + "T" + this.format("H:i:sP"); },
+    r: function() { return this.toString(); },
+    U: function() { return this.getTime() / 1000; }
+  };
+  for (var i = 0; i < format.length; i++) {
+    var curChar = format.charAt(i);
+    if (replace[curChar]) {
+      returnStr += replace[curChar].call(this);
+    } else {
+      returnStr += curChar;
+    }
+  }
+  return returnStr;
+};
+
 function encodeLongShout(text)
 {
   var b = '';
@@ -2860,8 +2925,9 @@ Pagehacks.prototype = {
           }
           with(insertCell(-1)) {
             className='row'+r;
-            innerHTML='<span class="gensmall">'+new Date(1000*pn.date).toLocaleString()+'</span>';
-            setAttribute('width','30');
+            var d = new Date(1000*pn.date);
+            innerHTML='<span class="gensmall">'+d.format("l")+'<br>'+d.format("d.m.y")+'<br>'+d.format("H:i")+'</span>';
+            setAttribute('width','50');
           }
         }
         r=r==1?2:1;
